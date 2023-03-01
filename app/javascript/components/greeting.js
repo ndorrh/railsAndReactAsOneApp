12e017d909975c750e46afacafa0bd4818a095b5
ndorrh <ndorrh@gmail.com>
2023-03-01 11:07:46 +0100

    Add greeting component to display greetings

---
+++ app/javascript/components/greeting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <div className="greeting">
+      <h2>Message</h2>
+      <p>Hi, I'm a React component!</p>
+    </div>
+  );
+}
+
+export default Greeting;
