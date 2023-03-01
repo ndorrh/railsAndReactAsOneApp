c3686aab477d1e63ecdffccf7dfa6cf804bc1cf8
ndorrh <ndorrh@gmail.com>
2023-03-01 11:28:44 +0100

    Create action to display message

---
+++ app/javascript/redux/messages.js
@@ -0,0 +1,27 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getMessages = createAsyncThunk(
+  'messageSlice/getMessages', async () => {
+    const response = await fetch('http://127.0.0.1:3000/messages')
+      .then((messages) => messages.json());
+      return response;
+  },
+);
+
+const messageSlice = createSlice({
+  name: 'message',
+  initialState: {
+    messages: [],
+    loading: true,
+    status: '',
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getMessages.fulfilled, (state, action) => {
+      const newState = state;
+      newState.messages = action.payload;
+    });
+  },
+});
+
+export default messageSlice;
