2af8561ac4aeeb3278544b7cef83f498e49b2a00
ndorrh <ndorrh@gmail.com>
2023-03-01 12:45:52 +0100

    Import store and provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Greeting from './components/greeting';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
@@ -12,18 +14,20 @@ function App() {
       <h2>React and Rails As one App</h2>
       <NavBar />
       <Routes>
-      <Route
-        path="/"
-        element={<Greeting />}
-      />  
-      </Routes> 
+        <Route
+          path="/"
+          element={<Greeting />}
+        />
+      </Routes>
     </div>
   );
 }
 
 ReactDOM.render(
   <Router>
-  <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root'),
 );
