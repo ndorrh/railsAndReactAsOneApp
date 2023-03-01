64af98ce1e1047f4dd925af3655ce392c90b7b03
ndorrh <ndorrh@gmail.com>
2023-03-01 14:48:41 +0100

    Fix linter error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def index
     def index
-    @message = Message.find(rand(1..Message.count))
-    render json: @message, status: 200
+      @message = Message.find(rand(1..Message.count))
+      render json: @message, status: 200
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources :messages, only: [:index]
+  resources :messages, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'root#index'
   # Defines the root path route ("/")
