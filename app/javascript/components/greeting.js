c1179f5cdd97e6a7dd64a3187665819f0303891b
ndorrh <ndorrh@gmail.com>
2023-03-01 15:09:56 +0100

    Update greetings component

--- app/javascript/components/greeting.js
+++ app/javascript/components/greeting.js
@@ -4,12 +4,19 @@ import { useSelector } from "react-redux";
 const Greeting = ({prop}) => {
 
    
-  if(prop != undefined) {
+  if(prop != undefined && prop != null) {
     return (
       <div className="greeting">
         <h2>Message</h2>
         <p><span>Text:</span> {prop.content}</p>
         <p><span>Created at:</span> {prop.created_at}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="greeting">
+        <h2>Message</h2>
+        <p>No message to display</p>
       </div>
     );
   }
