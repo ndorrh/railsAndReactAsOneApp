620be9e9f2e40f1fe4b8034ad006d16fcd3c0f38
ndorrh <ndorrh@gmail.com>
2023-03-01 14:58:29 +0100

    Fix linter errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    def index
-      @message = Message.find(rand(1..Message.count))
-      render json: @message, status: 200
-    end
+    @message = Message.find(rand(1..Message.count))
+    render json: @message, status: 200
   end
 end
